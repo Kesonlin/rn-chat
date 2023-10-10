629f7f678c628680e841abe5fc30a62f553869fb
linhaorui <2909753663@qq.com>
2023-10-10 15:23:05 +0800

    feat: 用户注册时添加头像

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -7,6 +7,9 @@ import {
   Dimensions,
   Pressable,
   Animated,
+  ImagePicker,
+  Platform,
+  Image,
 } from 'react-native';
 import {
   Container,
@@ -24,48 +27,90 @@ import {
   Link,
   VStack,
   useToast,
+  // Image,
 } from 'native-base';
+import {launchImageLibrary} from 'react-native-image-picker';
 import {request} from '../network';
+
 import store from '../store';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Register = props => {
+const Register = (props: any): JSX.Element => {
   console.log(props);
   const toast = useToast();
   const [info, setInfo] = useState({
     userName: '',
     password: '',
-    avatar: '',
+    // avatar: '',
   });
+  const [photo, setPhoto] = useState(null);
+
+  const handleChoosePhoto = () => {
+    launchImageLibrary({noData: true}, response => {
+      console.log('response', response);
+      if (response) {
+        setPhoto(response.assets[0]);
+      }
+    });
+  };
+
+  const createFormData = (photo, body = {}) => {
+    const data = new FormData();
+    console.log('photo', photo);
+    if (photo) {
+      data.append('avatar', {
+        name: photo.fileName,
+        type: photo.type,
+        uri:
+          Platform.OS === 'ios' ? photo.uri.replace('file://', '') : photo.uri,
+      });
+    }
+
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    });
+    console.log(' Platform.OS', Platform.OS);
+
+    console.log(data);
+
+    return data;
+  };
 
   const handleRegister = async () => {
-    const res: any = await request({
-      url: '/user/create',
-      method: 'post',
-      data: info,
-    });
-
-    const data = res?.data;
-
-    if (data && data.success) {
-      console.log('注册成功', data);
-      toast.show({
-        render: () => {
-          return (
-            <Box bg="emerald.500" px="2" py="1" rounded="sm" mb={5}>
-              注册成功！
-            </Box>
-          );
+    try {
+      const res: any = await request({
+        url: '/user/create',
+        method: 'post',
+        headers: {
+          'Content-Type': 'multipart/form-data',
         },
+        data: createFormData(photo, info),
       });
-      props.jumpTo('login');
-    } else {
-      Toast.show({
-        title: data.msg,
-        // type: 'danger',
-        placement: 'bottom',
-        duration: 3000,
-      });
+
+      const data = res?.data;
+
+      if (data && data.success) {
+        console.log('注册成功', data);
+        toast.show({
+          render: () => {
+            return (
+              <Box bg="emerald.500" px="2" py="1" rounded="sm" mb={5}>
+                注册成功！
+              </Box>
+            );
+          },
+        });
+        props.jumpTo('login');
+      } else {
+        Toast.show({
+          title: data.msg,
+          // type: 'danger',
+          placement: 'bottom',
+          duration: 3000,
+        });
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
 
@@ -92,6 +137,19 @@ const Register = props => {
           Sign up to continue!
         </Heading>
         <VStack space={3} mt="5">
+          {photo && (
+            <>
+              {/* <Text>{JSON.stringify(photo)}</Text> */}
+              <Image
+                source={{uri: photo.uri}}
+                style={{width: 100, height: 100}}
+              />
+              {/* <Button title="Upload Photo" onPress={handleUploadPhoto} /> */}
+            </>
+          )}
+          <Button title="Choose Photo" onPress={handleChoosePhoto}>
+            choose picker
+          </Button>
           <FormControl>
             <FormControl.Label>UserName</FormControl.Label>
             <Input
