63cfee5780715af3e03772591bc7a82c685a1007
linhaorui <2909753663@qq.com>
2023-10-14 20:05:16 +0800

    feat: 接入动态功能接口

--- src/pages/Moments.tsx
+++ src/pages/Moments.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   AddIcon,
   AspectRatio,
@@ -6,7 +6,6 @@ import {
   Box,
   Button,
   Center,
-  Divider,
   FormControl,
   HStack,
   Heading,
@@ -14,7 +13,6 @@ import {
   Image,
   Input,
   Modal,
-  Popover,
   ScrollView,
   Spacer,
   Stack,
@@ -22,9 +20,12 @@ import {
   VStack,
 } from 'native-base';
 import {launchImageLibrary} from 'react-native-image-picker';
-// const defaultPhoto = require('../assets/default-image.jpg');
-import defaultPhoto from '../assets/default-image.jpg';
-function Moment(): JSX.Element {
+import {request} from '../network';
+import {useIsFocused} from '@react-navigation/native';
+import store from '../store';
+import {Platform} from 'react-native';
+function Moment(props: any): JSX.Element {
+  const {item} = props;
   return (
     <Box alignItems="center" w="100%" borderColor="pink" mb="4">
       <Box
@@ -54,7 +55,7 @@ function Moment(): JSX.Element {
           <Avatar
             size="48px"
             source={{
-              uri: 'http://10.0.2.2:3000//henry1000000042.jpg',
+              uri: item.avatar,
             }}
           />
           <VStack>
@@ -64,109 +65,51 @@ function Moment(): JSX.Element {
               }}
               color="coolGray.800"
               bold>
-              {1111}
+              {item.userName}
             </Text>
             <Text
               color="coolGray.600"
               _dark={{
                 color: 'warmGray.200',
               }}>
-              {2222}
+              {item.createTime}
             </Text>
           </VStack>
           <Spacer />
-          {/* <Text
-          fontSize="xs"
-          _dark={{
-            color: 'warmGray.50',
-          }}
-          color="coolGray.800"
-          alignSelf="flex-start">
-          {'createTime'}
-        </Text> */}
         </HStack>
         <Box>
           <Stack space={3}>
             <Heading size="md" ml="3" mt="2" mb="2">
-              The Garden City
+              {item.title}
             </Heading>
-            {/* <Text
-              fontSize="xs"
-              _light={{
-                color: 'violet.500',
-              }}
-              _dark={{
-                color: 'violet.400',
-              }}
-              fontWeight="500"
-              ml="-0.5"
-              mt="-1">
-              The Silicon Valley of India.
-            </Text> */}
           </Stack>
           <AspectRatio w="100%" ratio={16 / 9}>
             <Image
               source={{
-                uri: 'https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg',
+                uri: item.photo,
               }}
               alt="image"
             />
           </AspectRatio>
-          {/* <Center
-            bg="violet.500"
-            _dark={{
-              bg: 'violet.400',
-            }}
-            _text={{
-              color: 'warmGray.50',
-              fontWeight: '700',
-              fontSize: 'xs',
-            }}
-            position="absolute"
-            bottom="0"
-            px="3"
-            py="1.5">
-            PHOTOS
-          </Center> */}
         </Box>
         <Stack p="4" space={3}>
           <Text fontWeight="400" fontSize="md">
-            Bengaluru (also called Bangalore) is the center of India's high-tech
-            industry. The city is also known for its parks and nightlife.
+            {item.content}
           </Text>
           <HStack alignItems="center" space={4} justifyContent="space-between">
-            <HStack alignItems="center">
-              {/* <Text
-                color="coolGray.600"
-                _dark={{
-                  color: 'warmGray.200',
-                }}
-                fontWeight="400">
-                6 mins ago
-              </Text> */}
-            </HStack>
+            <HStack alignItems="center"></HStack>
           </HStack>
         </Stack>
       </Box>
-      {/* <Divider
-        my="2"
-        h="2"
-        _light={{
-          bg: '#ccc',
-        }}
-        _dark={{
-          bg: 'muted.50',
-        }}
-      /> */}
     </Box>
   );
 }
 
-function AddMoment(): JSX.Element {
-  //   const defaultPhoto = require('../assets/default-image.jpg');
-
+function AddMoment(props: any): JSX.Element {
+  const {info, getMoments} = props;
   const [showModal, setShowModal] = useState(false);
   const [photo, setPhoto] = useState<any>();
+  const [moment, setMoment] = useState<any>();
 
   const handleChoosePhoto = () => {
     launchImageLibrary({mediaType: 'photo'}, response => {
@@ -181,7 +124,47 @@ function AddMoment(): JSX.Element {
       }
     });
   };
-  console.log('defaultPhoto', defaultPhoto);
+
+  const createFormData = (body: {[key: string]: any} = {}) => {
+    const data = new FormData();
+    console.log('photo', photo);
+    if (photo) {
+      data.append('photo', {
+        name: photo.fileName,
+        type: photo.type,
+        uri:
+          Platform.OS === 'ios' ? photo.uri.replace('file://', '') : photo.uri,
+      });
+    }
+
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    });
+    // console.log(' Platform.OS', Platform.OS);
+
+    console.log(data);
+
+    return data;
+  };
+
+  const addMoment = async () => {
+    console.log('moment', moment);
+    try {
+      await request({
+        url: '/moment/create',
+        method: 'post',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: info.token,
+        },
+        data: createFormData({...moment, createTime: new Date().toJSON()}),
+      });
+      setPhoto(null);
+      getMoments();
+    } catch (e) {}
+
+    setShowModal(false);
+  };
 
   return (
     <Center>
@@ -197,11 +180,25 @@ function AddMoment(): JSX.Element {
           <Modal.Body>
             <FormControl>
               <FormControl.Label>Title</FormControl.Label>
-              <Input />
+              <Input
+                onChangeText={value =>
+                  setMoment({
+                    ...moment,
+                    title: value,
+                  })
+                }
+              />
             </FormControl>
             <FormControl mt="3">
               <FormControl.Label>Content</FormControl.Label>
-              <Input />
+              <Input
+                onChangeText={value =>
+                  setMoment({
+                    ...moment,
+                    content: value,
+                  })
+                }
+              />
             </FormControl>
             <Image
               source={
@@ -229,12 +226,7 @@ function AddMoment(): JSX.Element {
                 }}>
                 Cancel
               </Button>
-              <Button
-                onPress={() => {
-                  setShowModal(false);
-                }}>
-                Save
-              </Button>
+              <Button onPress={addMoment}>Save</Button>
             </Button.Group>
           </Modal.Footer>
         </Modal.Content>
@@ -244,6 +236,46 @@ function AddMoment(): JSX.Element {
 }
 
 export default function () {
+  const isFouced = useIsFocused();
+  const [info, setInfo] = useState<userinfoType>();
+  const [momentList, setMomentList] = useState<any[]>([]);
+
+  const getMoments = async () => {
+    try {
+      const {data} = await request({
+        url: '/moment/get',
+        headers: {
+          Authorization: info.token,
+        },
+      });
+      console.log(data);
+      if (!data) return;
+      setMomentList(data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    if (!isFouced) return;
+
+    store
+      .load({
+        key: 'userInfo',
+      })
+      .then(res => {
+        console.log('storage1', res);
+        setInfo(res);
+      })
+      .catch(e => {
+        console.log('storage error', e);
+      });
+  }, [isFouced]);
+
+  useEffect(() => {
+    if (!isFouced || !info) return;
+    getMoments();
+  }, [isFouced, info]);
+
   return (
     <Box>
       <Stack
@@ -258,11 +290,12 @@ export default function () {
           Welcome to Moments !
         </Heading>
         {/* <Text> */}
-        <AddMoment />
+        <AddMoment info={info} getMoments={getMoments} />
       </Stack>
       <ScrollView h="90%">
-        <Moment />
-        <Moment />
+        {momentList.map(item => (
+          <Moment item={item} key={item.id} />
+        ))}
       </ScrollView>
     </Box>
   );
